dfb164f122f85e3f730b76a4da19ad07655629e4
Marius Krabset <mk@scienta.no>
2014-04-21 16:08:18 +0200

    Added sensortest-sender

---
+++ sensortest/sensortest-sender/src/main/java/no/scienta/sensortest/MainActivity.java
@@ -0,0 +1,147 @@
+package no.scienta.sensortest;
+
+import java.util.Timer;
+import java.util.TimerTask;
+
+import no.scienta.sensortest.cast.CastSession;
+import no.scienta.sensortest.cast.DiscoveryAndSelectHandler;
+import android.content.Context;
+import android.graphics.drawable.ColorDrawable;
+import android.hardware.SensorManager;
+import android.os.Bundle;
+import android.support.v4.view.MenuItemCompat;
+import android.support.v7.app.ActionBar;
+import android.support.v7.app.ActionBarActivity;
+import android.support.v7.app.MediaRouteActionProvider;
+import android.util.Log;
+import android.view.Menu;
+import android.view.MenuItem;
+
+import com.google.android.gms.cast.Cast.MessageReceivedCallback;
+import com.google.android.gms.cast.CastDevice;
+
+public class MainActivity extends ActionBarActivity {
+
+	private static final String TAG = MainActivity.class.getSimpleName();
+
+	private DiscoveryAndSelectHandler discoveryAndSelectHandler;
+	private CastSession castSession;	
+	private SensorStateSender sensorStateSender;
+
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.activity_main);
+		ActionBar actionBar = getSupportActionBar();
+		actionBar.setBackgroundDrawable(new ColorDrawable(android.R.color.transparent));
+
+		final String applicationId=getResources().getString(R.string.app_id);
+		final String namespace=getResources().getString(R.string.namespace);
+		
+		// Handler for Cast device discovery
+		this.discoveryAndSelectHandler=new DiscoveryAndSelectHandler(getApplicationContext(),applicationId) {
+			@Override
+			public void onConnected(CastDevice device) {
+				if (castSession!=null) {
+					castSession.close();
+				}
+				castSession=createSession(device, applicationId, namespace, getApplicationContext());
+			}
+			
+			@Override
+			public void onDisconnected() {
+				Log.i(TAG,"DiscoveryAndSelectHandler disconnected");
+				if (castSession!=null) {
+					castSession.close();
+					castSession=null;
+				}
+			}
+		};
+		
+		
+		sensorStateSender=new SensorStateSender((SensorManager)getSystemService(SENSOR_SERVICE)) {
+			
+			@Override
+			protected void send(String msg) {
+				if (castSession!=null && !castSession.isClosed()) {
+					Log.d(TAG,"Sending "+msg);
+					castSession.sendMessage(msg);
+				}
+			}
+		};		
+		Log.d(TAG, "Heisann");
+	}
+		
+	@Override
+	protected void onResume() {
+		super.onResume();
+		discoveryAndSelectHandler.startDiscovery();
+		sensorStateSender.start();
+		
+	}
+
+	@Override
+	protected void onPause() {
+		sensorStateSender.stop();
+		if (isFinishing()) {
+			discoveryAndSelectHandler.stopDiscovery();
+		}
+		super.onPause();
+	}	
+	
+	@Override
+	public boolean onCreateOptionsMenu(Menu menu) {
+		super.onCreateOptionsMenu(menu);
+		getMenuInflater().inflate(R.menu.main, menu);
+		MenuItem mediaRouteMenuItem = menu.findItem(R.id.media_route_menu_item);
+		MediaRouteActionProvider mediaRouteActionProvider = (MediaRouteActionProvider) MenuItemCompat.getActionProvider(mediaRouteMenuItem);
+		
+		// Set the MediaRouteActionProvider selector for device discovery.
+		mediaRouteActionProvider.setRouteSelector(discoveryAndSelectHandler.getRouteSelector());
+		return true;
+	}
+
+	
+	@Override
+	public void onDestroy() {
+		if (castSession!=null) {
+			castSession.close();
+		}
+		if (discoveryAndSelectHandler!=null) {
+			discoveryAndSelectHandler.close();
+		}
+		castSession=null;
+		discoveryAndSelectHandler=null;
+		super.onDestroy();
+	}
+
+
+	/**
+	 * Start the receiver app
+	 */
+	private CastSession createSession(CastDevice device, String applicationId, String namespace, Context context) {
+		try {
+			return new CastSession(device,applicationId,namespace,context) {
+				
+				@Override
+				protected void onClosed(CloseReason reason) {
+					Log.i(TAG,"onClosedSession");
+					
+				}
+				
+				@Override
+				protected void onApplicationStarted() {
+					Log.i(TAG,"onApplicationStarted");
+				}
+				
+				@Override
+				protected MessageReceivedCallback createNewMessageReceiver() {
+					return null;
+				}
+			};
+		} catch (Exception e) {
+			Log.e(TAG, "Failed launchReceiver", e);
+			return null;
+		}
+	}
+}
---
+++ sensortest/sensortest-sender/src/main/java/no/scienta/sensortest/cast/DiscoveryAndSelectHandler.java
@@ -0,0 +1,73 @@
+package no.scienta.sensortest.cast;
+
+import android.content.Context;
+import android.support.v7.media.MediaControlIntent;
+import android.support.v7.media.MediaRouteSelector;
+import android.support.v7.media.MediaRouter;
+import android.support.v7.media.MediaRouter.RouteInfo;
+import android.util.Log;
+
+import com.google.android.gms.cast.CastDevice;
+import com.google.android.gms.cast.CastMediaControlIntent;
+
+public abstract class DiscoveryAndSelectHandler {
+	private final String TAG="DiscoveryAndSelectHandler";
+	
+	private MediaRouter mediaRouter;
+	private final MediaRouter.Callback mediaRouterCallback;
+	private final MediaRouteSelector mediaRouteSelector;
+	private final String castCategory;
+	
+	
+	public DiscoveryAndSelectHandler(Context applicationContext, String applicationId) {
+		this.mediaRouter=MediaRouter.getInstance(applicationContext);
+		this.mediaRouterCallback=new MediaRouter.Callback() {
+			
+			@Override
+			public void onRouteSelected(MediaRouter router, RouteInfo info) {
+				Log.d(TAG, "onRouteSelected: "+info);
+				if (!isClosed()) { 
+					onConnected(CastDevice.getFromBundle(info.getExtras()));
+				}
+			}
+
+			@Override
+			public void onRouteUnselected(MediaRouter router, RouteInfo info) {
+				Log.d(TAG, "onRouteUnselected: " + info);
+				if (!isClosed()) {
+					onDisconnected();
+				}
+			}
+		};
+		this.castCategory = CastMediaControlIntent.categoryForCast(applicationId);
+		this.mediaRouteSelector = new MediaRouteSelector.Builder()
+		    .addControlCategory(castCategory)
+		    .build();
+	}
+	
+	public MediaRouteSelector getRouteSelector() {
+		return mediaRouteSelector;
+	}
+	
+	public void startDiscovery() {
+		mediaRouter.addCallback(mediaRouteSelector, mediaRouterCallback,MediaRouter.CALLBACK_FLAG_PERFORM_ACTIVE_SCAN);
+	}
+	
+	public void stopDiscovery() {
+		mediaRouter.removeCallback(mediaRouterCallback);
+	}
+	
+	public void close() {
+		if (!isClosed()) {
+			stopDiscovery();
+			mediaRouter=null;
+		}
+	}
+	
+	public boolean isClosed() {
+		return mediaRouter==null;
+	}
+		
+	public abstract void onConnected(CastDevice device);
+	public abstract void onDisconnected();
+}
